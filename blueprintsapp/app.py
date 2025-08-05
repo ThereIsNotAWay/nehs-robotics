a3ca5015857a3a0ebfc1741cf6e9f87efef091f7
ThereIsNotAWay <unitywillprevail@gmail.com>
2025-08-05 18:34:33 -0400

    Added app.py python file with a function that creates the flask app function, registers all flask blueprints, and has a global errorhandler for pages.

---
+++ blueprintsapp/app.py
@@ -0,0 +1,34 @@
+from os import environ
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+db = SQLAlchemy()
+
+def create_app():
+  app = Flask(__name__, template_folder='templates')
+
+  app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DATABASE_CONNECTION')
+  app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+  db.init_app(app)
+  
+  from blueprintsapp.blueprints.main.routes import main
+  from blueprintsapp.blueprints.news.routes import news
+  from blueprintsapp.blueprints.gallery.routes import gallery
+  from blueprintsapp.blueprints.resources.routes import resources
+
+  migrate = Migrate(app, db)
+
+  app.register_blueprint(main, url_prefix='/')
+  app.register_blueprint(news, url_prefix='/news')
+  app.register_blueprint(gallery, url_prefix='/gallery')
+  app.register_blueprint(resources, url_prefix='/resources')
+
+  @app.errorhandler(404)
+  def global_404(e):
+    return render_template('wip_page.html')
+
+  return app
+
+
