c035c9350d127c56879bd128174bd029b60c699a
ThereIsNotAWay <unitywillprevail@gmail.com>
2025-08-17 18:21:37 -0400

    Added changes to routes.py to refer mainly to its own flask resources blueprint when applicable.

--- blueprintsapp/blueprints/resources/routes.py
+++ blueprintsapp/blueprints/resources/routes.py
@@ -1,6 +1,6 @@
 from flask import request, render_template, redirect, url_for, Blueprint
 
-resources = Blueprint('resources', __name__, template_folder='templates')
+resources = Blueprint('resources', __name__, template_folder='templates', static_folder='static', static_url_path='resources/static')
 
 @resources.route('/')
 def all_resources():
