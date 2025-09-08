ab0f556e448131824b07d5470d254b7615dc3681
ThereIsNotAWay <unitywillprevail@gmail.com>
2025-09-08 17:33:33 -0400

    Implemented JavaScript page function routes. (Are subject to change after front-end overhaul.)

---
+++ functions/api.js
@@ -0,0 +1,31 @@
+export async function fetchPage(context) {
+  const { env, request } = context;
+
+  const baseUrl = env.API_CONNECTION;
+
+  const url = new URL(request.url);
+  const path = url.pathname;
+
+  try {
+    const routes = ['/', '/about', '/news', '/gallery', '/resources', '/contacts']
+
+    if (!response.ok) {
+      return new Response('Error fetching page or page does\'t exist', { status: 400 });
+    } 
+
+    const page = await response.text()
+    const modifiedPage = page.replace(
+          /"(\/?[^"]+)(\.(css|js|png|jpg|jpeg|gif|svg))"/g,
+          (match, filePath, fileType) => (!filePath.includes('splinetool')) ? `"${baseUrl}${filePath}${fileType}"` : `${filePath}${fileType}`
+        );
+
+    return new Response(modifiedPage, {
+      headers: { 'Content-Type': 'text/html' }
+    });
+
+  }
+  catch(error) {
+    return new Response('Error fetching the page', { status: 500 });
+  }
+
+}
---
+++ functions/resources.js
@@ -0,0 +1,7 @@
+import { fetchPage } from './api'
+
+export async function onRequest(context) {
+  
+  const targetPage = await fetchPage(context);
+  return targetPage;
+}
