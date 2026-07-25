37f61067b326de4fb4698ba1528a26725db49aee
ThereIsNotAWay <unitywillprevail@gmail.com>
2026-07-25 19:14:11 -0400

    Created auth blueprint to handle authentication like login and models to store users.

---
+++ api/blueprints/auth/models.py
@@ -0,0 +1,22 @@
+from sqlalchemy import Column, String, text
+from sqlalchemy.dialects.postgresql import UUID
+from pydantic import BaseModel, Field
+
+from api.database import Base
+
+class User(Base):
+
+    __tablename__ = "users"
+
+    id = Column(UUID(as_uuid=True), primary_key=True, server_default=text("gen_random_uuid()"))
+    name = Column(String, nullable=False, unique=True)
+    password_hash = Column(String, nullable=False)
+    role = Column(String, nullable=False, server_default="User")
+
+class LoginRequest(BaseModel):
+    name: str = Field(min_length=8, max_length=20)
+    password: str = Field(min_length=12, max_length=128)
+
+class SignupRequest(BaseModel):
+    name: str = Field(min_length=8, max_length=20)
+    password: str = Field(min_length=12, max_length=128)
---
+++ api/blueprints/auth/routes.py
@@ -0,0 +1,80 @@
+from flask import Blueprint, request, jsonify
+from flask_login import login_user, current_user, logout_user
+from api.extensions import bcrypt, limiter, login_manager
+from api.database import SessionLocal
+from api.blueprints.auth.models import User, LoginRequest, SignupRequest
+from pydantic import ValidationError
+from sqlalchemy import select
+from werkzeug.exceptions import BadRequest
+
+auth = Blueprint('auth', __name__)
+
+@login_manager.user_loader
+def load_user(user_id):
+  with SessionLocal() as session:
+    return session.scalar(select(User).where(User.id==user_id))
+
+@auth.route('/logout')
+def logout():
+  logout_user()
+  return jsonify({"success": True}), 200
+
+@auth.route('/login', methods=['POST'])
+@limiter.limit("5 per minute")
+def login():
+  try: 
+    if not request.is_json:
+      return jsonify({"success": False, "message": "Invalid Json"}), 415
+
+    login_form = request.get_json()
+    login_attempt = LoginRequest.model_validate(login_form)
+
+    name = login_attempt.name.strip().lower()
+    password = login_attempt.password
+    
+    with SessionLocal() as session:
+      user = session.query(User).filter_by(name=name).first()
+      if not user or not bcrypt.check_password_hash(user.password, password):
+        return jsonify({"success": False, "message": "Username or password is invalid!"}), 401
+
+      login_user(user)
+      return jsonify({"success": True, "message": "Success!"}), 200
+
+  except ValidationError as e:
+    return jsonify({"success": False, "message": "Invalid credientials!"}), 400
+
+  except BadRequest:
+    return jsonify({"success": False, "message": "Malformed Json"})
+
+@auth.route('/signup', methods=['POST'])
+@limiter.limit("5 per minute")
+def signup():
+
+  try: 
+    if not request.is_json:
+      return jsonify({"success": False, "message": "Invalid Json"}), 415
+
+    signup_form = request.get_json()
+    signup_attempt = SignupRequest.model_validate(signup_form)
+  
+    name = signup_attempt.user.strip().lower()
+    password = signup_attempt.password
+        
+    with SessionLocal() as session:
+      user_exists = session.scalar(select(User).where(User.name==name))
+      if user_exists:
+        return jsonify({"success": False, "message": "Unable to create account!"}), 409
+
+      hashed_password = bcrypt.generate_password_hash(password).decode("utf-8")
+      new_user = User(name=name, password=hashed_password)
+
+      session.add(new_user)
+      session.commit()  
+      
+      return jsonify({"success": True, "message": "Success!"}), 201
+  
+  except ValidationError as e:
+    return jsonify({"success": False, "message": "Invalid inputs!"}), 401
+
+  except BadRequest:
+      return jsonify({"success": False, "message": "Malformed Json"}), 400
