23ae2fe6027046ce3b402536109d59711aed9cc9
ThereIsNotAWay <unitywillprevail@gmail.com>
2026-08-28 22:51:47 -0400

    Updated vite.config.js to include an environment variable for backend url and a default value for local development.

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,12 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+const backendAPI = process.env.VITE_BACKEND_URL || 'http://backend:8000';
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     proxy: {
-      '/api': 'http://localhost:5000',
+      '/api': backendAPI,
     },
   },
 })
